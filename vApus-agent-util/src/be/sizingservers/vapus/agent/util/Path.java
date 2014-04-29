74554a233492fe2ea2d1dd26eac53e7b7af863e0
Dieter Vandroemme <dieter@sizingservers.be>
2014-04-29 11:02:30 +0200

    Small delimiter bug fix for Path.java and added an exception if the determined jar path does not end with.jar (compile misconfig)

--- vApus-agent-util/src/be/sizingservers/vapus/agent/util/Path.java
+++ vApus-agent-util/src/be/sizingservers/vapus/agent/util/Path.java
@@ -21,8 +21,9 @@ public class Path {
      * a / or a \ (OS specific).
      *
      * @return An empty String if the jar path could not be determined.
+     * @throws java.lang.Exception The determined jar path does not end with .jar, please check the compile properties for your app.
      */
-    public static String getJarParrentPath() {
+    public static String getJarParrentPath() throws Exception {
         String jarPath = getJarPath(true);
         if (jarPath.length() == 0) {
             return "";
@@ -41,25 +42,36 @@ public class Path {
     }
 
     /**
-     * Gets the path of the current executing jar, filename included. Does not end with the delimiter.
-     * @return 
+     * Gets the path of the current executing jar, filename included. Does not
+     * end with the delimiter.
+     *
+     * @return
+     * @throws java.lang.Exception The determined path does not end with .jar, please check the compile properties for your app.
      */
-    public static String getJarPath(){
+    public static String getJarPath() throws Exception {
         return getJarPath(false);
     }
+
     /**
      * Gets the path of the current executing jar, filename included.
      *
      * @param endWithDelimiter
      * @return An empty String if the jar path could not be determined.
+     * @throws java.lang.Exception The determined path does not end with .jar, please check the compile properties for your app.
      */
-    public static String getJarPath(boolean endWithDelimiter) {
+    public static String getJarPath(boolean endWithDelimiter) throws Exception {
         try {
             CodeSource codeSource = Path.class.getProtectionDomain().getCodeSource();
-            String jarPath = codeSource.getLocation().toURI().getPath();
-            
+
+            File jarFile = new File(codeSource.getLocation().toURI().getPath());
+            String jarPath = jarFile.getAbsolutePath();
+
+            String delimiter = extractDelimiter(jarPath);
+            if (!jarPath.endsWith(".jar") && !jarPath.endsWith(".jar" + delimiter)) {
+                throw new Exception("The determined path does not end with .jar, please check the compile properties for your app. (" + jarPath + ")");
+            }
+
             if (endWithDelimiter) {
-                String delimiter = extractDelimiter(jarPath);
                 if (!jarPath.endsWith(delimiter)) {
                     jarPath += delimiter;
                 }
@@ -75,8 +87,9 @@ public class Path {
      * Gets the OS specific path delimiter.
      *
      * @return An empty String if the jar path could not be determined.
+     * @throws java.lang.Exception The determined path does not end with .jar, please check the compile properties for your app.
      */
-    public static String getDelimiter() {
+    public static String getDelimiter() throws Exception {
         String jarPath = getJarPath(true);
         return "" + jarPath.charAt(jarPath.length() - 1);
 
